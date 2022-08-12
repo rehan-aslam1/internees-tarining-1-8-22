09d6cfb785ec2a4e2b1a91c7a2eddd139391c444
Rehan Aslam <rehan.dev@pk.see.biz>
2022-08-12 12:17:12 +0500

    style handleClick props state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import Counter from "./Components/Counter";
+import './App.css'
 class App extends Component {
-  state = {};
+  state = {
+    heading:'Counter component props'
+  };
   render() {
+    const {heading} = this.state;
     return (
       <>
         <h1>Learning react js by class component</h1>
-        <Counter />
+        <Counter heading={heading} />
       </>
     );
   }
--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -4,11 +4,19 @@ class Counter extends Component {
   state = {
     count: 0,
   };
+  handleClick = () => {
+    this.setState({
+      count:this.state.count + 1
+    });
+  }
   render() {
+    const style ={marginLeft:"50px", backgroundColor:'blue',color:'white'};
     const { count } = this.state;
+    const { heading  } = this.props;
     return (
       <>
-        <h1>Counter component Counts {count}</h1>
+        <h1>{heading} {count}</h1><br/>
+        <button className="btn-style" onClick={this.handleClick}>Click count</button>
       </>
     );
   }
