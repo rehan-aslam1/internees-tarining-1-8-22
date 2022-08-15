c4963f450700df19dcc2e3ea8f05b1ddc1b1669f
Rehan Aslam <rehan.dev@pk.see.biz>
2022-08-15 14:26:11 +0500

    this issue, conditional redenring, setState

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,21 +1,36 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  // constructor(props){
+  //   super(props);
+  //   this.handleClick = this.handleClick.bind(this)   
+  // }
   state = {
     count: 0,
   };
+
   handleClick = () => {
-    this.setState({
-      count:this.state.count + 1
-    });
+  //  this.state.count +=1;
+  this.setState({
+    count:this.state.count + 1
+  });
+  // const {count} = this.state;
+  // this.setState({
+  //   count:count + 1
+  // });
+  // this.setState(prevState =>{
+  //   return{
+  //     count:prevState.count + 1
+  //   } 
+  // });
   }
   render() {
-    const style ={marginLeft:"50px", backgroundColor:'blue',color:'white'};
+    // const style ={marginLeft:"50px", backgroundColor:'blue',color:'white'};
     const { count } = this.state;
     const { heading  } = this.props;
     return (
       <>
-        <h1>{heading} {count}</h1><br/>
+        <h1>{heading} {count===0 ? 'zero': count }</h1><br/> 
         <button className="btn-style" onClick={this.handleClick}>Click count</button>
       </>
     );
